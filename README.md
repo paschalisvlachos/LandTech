f3fce1d23b44808a01e9aa9ad7a0b955e884010b
Paschalis Vlachos <paschalis.vlachos@and.digital>
2021-03-11 11:21:35 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+<!-- How to run the codebase -->
+
+Initially run `npm install` to install the node_modules
+To run the tasks, run the below
+
+First Task:
+`node run.js --searchLandId "T100030485625"`
+
+Second Task:
+`node run.js --searchCompanyId "R602666366674"`
---
+++ first.js
@@ -0,0 +1,66 @@
+function first(searchLandId){
+  const fs = require("fs");
+  var argv = require("optimist").argv;
+
+  var recur = function (id,pId){
+    if(pId != ""){
+        id=pId;
+        fs.readFileSync("./company_relations.csv","utf8")
+        .split("\n")
+        .slice(1)
+        .forEach((line) => {
+            const [companyId,name,parentId] = line.split(",");
+
+            if(companyId == id)
+            {
+              recur(companyId,parentId);
+              if(parentId != "")
+                console.log("-"+name);
+              else
+                console.log(name);
+            }
+        });
+      }
+      else{
+          return id;
+      }
+    }
+
+  fs.readFileSync("./land_ownership.csv", "utf8")
+    .split("\n")
+    .slice(1) // header row
+    .forEach((line) => {
+      const [landId, companyId] = line.split(",");
+      if(landId == searchLandId) {
+
+          fs.readFileSync("./company_relations.csv", "utf8")
+              .split("\n")
+              .slice(1) // header row
+              .forEach((line,index) => {
+                  const [id, name, parentId] = line.split(",");
+                  if(id == companyId) {
+                      recur(id,parentId);
+                      if(parentId != ""){
+                        console.log("-"+name);
+                      }
+                      else{
+                        console.log(name);
+                      }
+                  }
+                });
+      }
+  });
+
+  return 1;
+}
+
+module.exports = first;
+
+
+
+
+
+
+
+
+
